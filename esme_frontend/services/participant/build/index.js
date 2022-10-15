2d776668eb0fbdc2c92f98d298057de5f347c708
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2022-10-16 00:45:58 +0200

    Added blank expo project with TypeScript

--- esme_frontend/services/participant/build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const common_1 = require("@esme_frontend/common");
-console.log(common_1.projectName);
-//# sourceMappingURL=index.js.map
--- esme_frontend/services/participant/src/index.ts
+++
@@ -1,3 +0,0 @@
-import {projectName} from "@esme_frontend/common";
-
-console.log(projectName);
