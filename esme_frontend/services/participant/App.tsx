62f186739eee805ddd451ebc3ba14a64a71c1d24
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2022-10-16 18:42:27 +0200

    Added eslint to React Native

--- esme_frontend/services/participant/App.tsx
+++ esme_frontend/services/participant/App.tsx
@@ -1,22 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { projectName } from '@esme_frontend/common';
+import React from 'react'
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { projectName } from '@esme_frontend/common'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Project name: {projectName}</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export const App = (): JSX.Element => (
+  <View style={styles.container}>
+    <Text>Project name: {projectName}</Text>
+    <Text>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto"/>
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
