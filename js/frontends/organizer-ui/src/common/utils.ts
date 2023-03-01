3466c375c67f88d183dcdfd7fade6e3eab5ee5f0
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2023-03-01 23:16:28 +0100

    changelog details view

--- js/frontends/organizer-ui/src/common/utils.ts
+++ js/frontends/organizer-ui/src/common/utils.ts
@@ -5,10 +5,13 @@ import { GridValueGetterParams } from '@mui/x-data-grid/models/params/gridCellPa
 dayjs.extend(utc)
 const utcdayjs = (date?: dayjs.ConfigType): dayjs.Dayjs => dayjs(date).utc()
 
-export const parseDateTimeColumn = ({ value }: GridValueGetterParams<string>): string =>
+export const parseDateTimeLabel = (value?: string): string =>
   utcdayjs(value).format('YYYY-MM-DD HH:mm')
-export const parseDateTimeValue = (value: string): string =>
+export const parseDateTimeValue = (value?: string): string =>
   utcdayjs(value).format('YYYY-MM-DDTHH:mm')
+
+export const parseDateTimeColumn = ({ value }: GridValueGetterParams<string>): string =>
+  parseDateTimeLabel(value)
 
 export const parseErrorMessage = (message: string[] | string | undefined): string[] => {
   if (message === undefined) {
--- js/frontends/organizer-ui/src/pages/changelogs/changelog-details.view.tsx
+++ js/frontends/organizer-ui/src/pages/changelogs/changelog-details.view.tsx
@@ -2,12 +2,13 @@ import React, { Fragment } from 'react'
 import { useParams } from 'react-router-dom'
 import Header from '../../layout/header'
 import { AlertBar } from '../../common/alert-bar/alert-bar.component'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { styles } from '../../layout/styles'
 import Paper from '@mui/material/Paper'
 import { CardTitle } from '../../common/card-title.component'
 import { JsonViewer } from '@textea/json-viewer'
 import { useChangelog } from './changelog.entity'
+import { parseDateTimeLabel } from '../../common/utils'
 
 export const ChangelogDetailsView = (): JSX.Element => {
   const { id: idFromRoute } = useParams()
@@ -26,15 +27,43 @@ export const ChangelogDetailsView = (): JSX.Element => {
 }
 
 const ChangelogDetailsCard = ({ id }: { id: number }): JSX.Element => {
-  const { errorMessages, entity } = useChangelog(id)
-  console.log(errorMessages)
+  const { entity: changelog } = useChangelog(id)
 
-  if (entity === undefined) {
+  if (changelog === undefined) {
     return <></>
   }
 
   return <Paper sx={styles.layout.cardMedium}>
     <CardTitle title='Changelog details' entityName='changelog' listUrl='/changelogs'/>
-    <JsonViewer value={entity.valueAfter}/>
+
+    <ChangelogField name='action' value={changelog.type} bold={true}/>
+    <ChangelogField name='id' value={changelog.id}/>
+    <ChangelogField name='entity' value={`${changelog.entityClass} ${changelog.entityId}`}/>
+    <ChangelogField name='user id' value={changelog.userId}/>
+    <ChangelogField name='time' value={parseDateTimeLabel(changelog.changedAt)}/>
+
+    {changelog.valueAfter == null
+      ? <></>
+      : <Box sx={{ backgroundColor: '#f8fafa', my: 2, p: 2 }}>
+        <JsonViewer rootName={false} value={JSON.parse(changelog.valueAfter)}/>
+      </Box>
+    }
   </Paper>
+}
+
+interface ChangelogFieldProps {
+  name: string
+  value: number | string | undefined
+  bold?: boolean
+}
+
+const ChangelogField = ({ name, value, bold = false }: ChangelogFieldProps): JSX.Element => {
+  const fontWeight = bold ? 'bolder' : 'normal'
+
+  return <Box>
+    <Box sx={{ width: '80px', display: 'inline-block' }}>
+      <Typography variant='overline' fontWeight={fontWeight}>{name}:</Typography>
+    </Box>
+    <Typography component='span' fontWeight={fontWeight}>{value}</Typography>
+  </Box>
 }
