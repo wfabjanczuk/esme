0107cab53e7b1781e272c7cbfb132cd32349bb57
Wojciech Fabjańczuk <wojciechfabjanczuk@gmail.com>
2023-05-06 20:47:11 +0200

    sending problem description

--- go/services/messenger-api/internal/modules/ws/consumers/organizers/consume_start_chat.go
+++ go/services/messenger-api/internal/modules/ws/consumers/organizers/consume_start_chat.go
@@ -1,10 +1,14 @@
 package organizers
 
 import (
+	"fmt"
 	"messenger-api/internal/modules/common"
+	"messenger-api/internal/modules/infrastructure/chat_requests"
 	"messenger-api/internal/modules/infrastructure/chats"
+	"messenger-api/internal/modules/infrastructure/messages"
 	"messenger-api/internal/modules/ws/connections"
 	"messenger-api/internal/modules/ws/protocol/out"
+	"time"
 )
 
 func (c *Consumer) consumeStartChat(msg *connections.OrganizerMessage) {
@@ -41,18 +45,38 @@ func (c *Consumer) consumeStartChat(msg *connections.OrganizerMessage) {
 	c.chatsManager.SetChatCache(chat.Id, chat.OrganizerId, chat.ParticipantId, chat.EventId)
 
 	enrichedChat := c.enrichedChatsService.EnrichWithParticipant([]*chats.Chat{chat})[0]
-	outMsg, err := out.BuildNewEnrichedChat(enrichedChat)
+	newEnrichedChatMsg, err := out.BuildNewEnrichedChat(enrichedChat)
 	if err != nil {
 		c.logger.Printf("could not send %s to chat %d: %s\n", out.MsgTypeNewChat, chat.Id, err)
 		msg.Source.SendError(common.ErrInternal)
 		return
 	}
 
-	err = c.chatsManager.SendProtocolMessageToChat(chat, outMsg)
+	err = c.chatsManager.SendProtocolMessageToChat(chat, newEnrichedChatMsg)
 	if err != nil {
 		c.logger.Printf("could not send %s to chat %d: %s\n", out.MsgTypeNewChat, chat.Id, err)
 		msg.Source.SendError(common.ErrInternal)
 		return
 	}
+
+	if chatRequest.Description != "" {
+		go c.sendProblemDescription(chatRequest, chat.Id)
+	}
+
 	c.logger.Printf("organizer %d started new chat %s\n", organizer.Id, chat.Id)
+}
+
+func (c *Consumer) sendProblemDescription(chatRequest *chat_requests.ChatRequest, chatId string) {
+	participantMessage := &messages.Message{
+		ChatId:        chatId,
+		Content:       fmt.Sprintf("Problem description: %s", chatRequest.Description),
+		FromOrganizer: 0,
+		AuthorId:      chatRequest.ParticipantId,
+		TimeSent:      time.Now(),
+	}
+
+	err := c.chatsManager.SendUserMessageToChat(chatId, participantMessage)
+	if err != nil {
+		c.logger.Printf("could not send problem description to chat %s: %s\n", chatId, err)
+	}
 }
